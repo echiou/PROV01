7f7b86bd009e7ba34ff7e281edad75c6eea8554b
Ethan Chiou <ethanchiou@berkeley.edu>
2016-02-09 21:40:37 -0800

    simple fading (I think it works)

--- prov1_feather/prov1_feather.ino
+++ prov1_feather/prov1_feather.ino
@@ -4,14 +4,26 @@
 #define NP_PIN 6
 #define POT_ONE 7
 
+unsigned long time; //Keep track of the time
+unsigned long stripArr[NUM_PIXELS]; //Time data for strip (when to turn them on/off, etc.)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, NP_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
+  memset(stripArr, 0, sizeof(stripArr));
+
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //Fade all at once
+  time = millis(); //We'll need this for when different things start fading differently
+  for (int i = 0; i < NUM_PIXELS; i++) {
+    int whiteLevel = time - stripArr[i]; //Flashing white, so all the same
+    strip.setPixelColor(i, strip.Color(whiteLevel, whiteLevel, whiteLevel));
+    if (whiteLevel >= 255) { //Reset when it's fully white (Change when we do colours.)
+      stripArr[i] = time;
+    }
+  }
+  strip.show();
 }
