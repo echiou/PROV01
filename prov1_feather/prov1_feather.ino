554e2714f984d231e2a6d6e2d9f7915bd370ad2d
Angira Shirahatti <ashirahattia@berkeley.edu>
2016-02-10 00:04:41 -0800

    adjusted fade function to work with delay

--- prov1_feather/prov1_feather.ino
+++ prov1_feather/prov1_feather.ino
@@ -7,6 +7,7 @@
 unsigned long time; //Keep track of the time
 unsigned long stripArr[NUM_PIXELS]; //Time data for strip (when to turn them on/off, etc.)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, NP_PIN, NEO_GRB + NEO_KHZ800);
+int whiteLevel = 0;
 
 void setup() {
   memset(stripArr, 0, sizeof(stripArr));
@@ -16,14 +17,25 @@ void setup() {
 }
 
 void loop() {
+  //This is a global variable so that we can just rely on loop() to repeat behavior
+  whiteLevel = whiteLevel % 255;
+  for (int i = 0; i < NUM_PIXELS; i++) { 
+    strip.setPixelColor(i, whiteLevel, whiteLevel, whiteLevel);
+  }
+  whiteLevel = whiteLevel + 10;
+  delay(100); // Delay can be adjusted as we like
+  strip.show();
+
+  /*
   //Fade all at once
   time = millis(); //We'll need this for when different things start fading differently
+  int whiteLevel = 0;
   for (int i = 0; i < NUM_PIXELS; i++) {
-    int whiteLevel = time - stripArr[i]; //Flashing white, so all the same
+    whiteLevel = time - stripArr[i]; //Flashing white, so all the same
     strip.setPixelColor(i, strip.Color(whiteLevel, whiteLevel, whiteLevel));
     if (whiteLevel >= 255) { //Reset when it's fully white (Change when we do colours.)
       stripArr[i] = time;
     }
   }
-  strip.show();
+  */
 }
