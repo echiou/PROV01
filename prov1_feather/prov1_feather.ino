07adde70c6e1e9aee25f7f17614ab51827f48c1c
Ethan Chiou <ethanchiou@berkeley.edu>
2016-02-10 02:16:33 -0800

    I think I got it

--- prov1_feather/prov1_feather.ino
+++ prov1_feather/prov1_feather.ino
@@ -7,8 +7,9 @@
 unsigned long time; //Keep track of the time
 unsigned long stripArr[NUM_PIXELS]; //Time data for strip (when to turn them on/off, etc.)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, NP_PIN, NEO_GRB + NEO_KHZ800);
-int whiteLevel = 0;
-bool up = true;
+int delayTime = 1000;
+//int whiteLevel;
+//bool up = true;
 
 void setup() {
   memset(stripArr, 0, sizeof(stripArr));
@@ -19,42 +20,46 @@ void setup() {
 
 void loop() {
   //This is a global variable so that we can just rely on loop() to repeat behavior
-  if (whiteLevel >= 240) {
-    up = false;
-  }
-  else if (whiteLevel < 10) {
-    up = true;
-  }
+//  if (whiteLevel >= 240) {
+//    up = false;
+//  }
+//  else if (whiteLevel < 10) {
+//    up = true;
+//  }
   
-  /*
-  if (up){
-   whiteLevel = whiteLevel % 255; 
-  }
-  else {
-    whiteLevel = 255 - (whiteLevel % 255);
-  }
-  */
-  for (int i = 0; i < NUM_PIXELS; i++) { 
-    strip.setPixelColor(i, whiteLevel, whiteLevel, whiteLevel);
-  }
-  if (up) {
-    whiteLevel = whiteLevel + 10;
-  } else {
-    whiteLevel = whiteLevel - 10;
-  }
-  delay(100); // Delay can be adjusted as we like
-  strip.show();
+//  if (up){
+//   whiteLevel = whiteLevel % 255; 
+//  }
+//  else {
+//    whiteLevel = 255 - (whiteLevel % 255);
+//  }
+  
+//  for (int i = 0; i < NUM_PIXELS; i++) { 
+//    strip.setPixelColor(i, whiteLevel, whiteLevel, whiteLevel);
+//  }
+//  if (up) {
+//    whiteLevel = whiteLevel + 10;
+//  } else {
+//    whiteLevel = whiteLevel - 10;
+//  }
+//  delay(100); // Delay can be adjusted as we like
+//  strip.show();
 
-  /*
-  //Fade all at once
-  time = millis(); //We'll need this for when different things start fading differently
-  int whiteLevel = 0;
+  delayTime = 1000; //We'll change this when we get an accelerometer.
+  double delayTimeOffset = double(delayTime) / 2
+  time = millis();
+  double whiteLevel;
   for (int i = 0; i < NUM_PIXELS; i++) {
-    whiteLevel = time - stripArr[i]; //Flashing white, so all the same
-    strip.setPixelColor(i, strip.Color(whiteLevel, whiteLevel, whiteLevel));
-    if (whiteLevel >= 255) { //Reset when it's fully white (Change when we do colours.)
+    double diff = time - stripArr[i] - delayTimeOffset; //Flashing white, so all the same
+    if(diff < 0) { //Increase brightness
+      whiteLevel = -diff / delayTimeOffset * 255;
+    } else {
+      whiteLevel = diff / delayTimeOffset * 255;
+    }
+    strip.setPixelColor(i, strip.Color((int)whiteLevel, (int)whiteLevel, (int)whiteLevel));
+    if (diff >= delayTimeOffset) { //Reset after a full cycle
       stripArr[i] = time;
     }
   }
-  */
+  delay(50); //This should be kept constant
 }
