1d7618ccafcd782a482b1a9fa11f02fb0e1f36d9
Ethan Chiou <ethanchiou@berkeley.edu>
2016-02-11 14:39:10 -0800

    did randomness, accidentally added some colour code

--- prov1_feather/prov1_feather.ino
+++ prov1_feather/prov1_feather.ino
@@ -2,14 +2,23 @@
 
 #define NUM_PIXELS 4
 #define NP_PIN 6
-#define POT_ONE 7
+#define POT1_PIN A1
 
 unsigned long time; //Keep track of the time
+
+// Neopixel randomness
 unsigned long stripArr[NUM_PIXELS]; //Time data for strip (when to turn them on/off, etc.)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, NP_PIN, NEO_GRB + NEO_KHZ800);
 int onTime = 1000;
-//int whiteLevel;
-//bool up = true;
+long randOn = 50; // ~5% of cycles, turn on random pixel
+
+// Potentiometer & Colors
+int val1;
+int green[3] = {0, 255, 0};
+int blue[3] = {0, 0, 255};
+int purple[3] = {255, 0, 255};
+int red[3] = {255, 0, 0};
+int yellow[3] = {255, 255, 0}; 
 
 void setup() {
   memset(stripArr, 0, sizeof(stripArr));
@@ -18,31 +27,42 @@ void setup() {
   strip.show(); // Initialize all pixels to 'off'
   
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
   Serial.println("Serial Up");
 }
 
 void loop() {
   onTime = 1000; //We'll change this when we get an accelerometer.
+  randOn = 50; //This should also shift with accelerometer maybe.
   time = millis();
+  
+  val1 = analogRead(POT1_PIN); // value for first potentiometer
+  double ratio1 = (double) (val1 / 1023.0); // used to determine position between hues
+  
   double whiteLevel;
   for (int i = 0; i < NUM_PIXELS; i++) {
     double diff = time - stripArr[i]; //Time always >= set stripArrs
-    if(diff < (double)onTime / 2) { //Increase brightness
-      whiteLevel = diff / ((double)onTime / 2) * 255;
-    } else { //Decrease
-      whiteLevel = ((double)onTime - diff) / ((double)onTime / 2) * 255;
+    double halfOnTime = (double)onTime / 2;
+    if(diff < halfOnTime) { //Increase brightness
+      whiteLevel = diff / (halfOnTime) * 255;
+    } else if  (diff >= halfOnTime && diff < onTime) { //Decrease
+      whiteLevel = ((double)onTime - diff) / (halfOnTime) * 255;
     }
-    if (whiteLevel < 10) {
-      whiteLevel = 10;
+    else {
+      whiteLevel = NULL;
+      if (random(0, 1000) < randOn) {
+        stripArr[i] = time;
+      }
     }
-    strip.setPixelColor(i, strip.Color((int)whiteLevel, (int)whiteLevel, (int)whiteLevel));
-    if (diff >= onTime) { //Reset after a full cycle
-      stripArr[i] = time;
+    if (whiteLevel) {
+      if (whiteLevel < 20) {
+        whiteLevel = 20;
+      }
+      strip.setPixelColor(i, strip.Color((int)whiteLevel, (int)whiteLevel, (int)whiteLevel));
+    }
+    else {
+      strip.setPixelColor(i, strip.Color(0,0,0));
     }
   }
   strip.show();
-  delay(100); //This should be kept constant
+  delay(50); //This should be kept constant
 }
